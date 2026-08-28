b24dcc9b592d8c3768da274b2c653eab7d28d9cb
chepa <89663997+Chepa1@users.noreply.github.com>
2026-08-28 15:15:58 -0400

    players can move and jump

--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
+@export var move_speed: float = 5
+@export var jump_speed: float = 7
+@export var acceleration: float = 20
+
 @onready var camera_3d: Camera3D = $Head/Camera3D
 @onready var label_3d: Label3D = $Label3D
-
-#const SPEED = 5.0
-#const JUMP_VELOCITY = 4.5
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("test"):
@@ -18,24 +19,30 @@ func setup(player_data: Statics.PlayerData) -> void:
 @rpc("authority", "call_local", "unreliable_ordered")
 func test() -> void:
 	Debug.log("meh")
-#func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	#move_and_slide()
+	
+func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	if not is_on_floor(): #Agrega gravedad
+		velocity += get_gravity() * delta
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"): #salto
+		velocity.y = jump_speed
+		
+	var move_input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
+	var direction: Vector3 = transform.basis * Vector3(move_input.x, 0, move_input.y) #Aca hago que si me muevo a la derecha, sea la derecha del jugador, no del mundo.
+	var target: Vector2 = Vector2(direction.x, direction.z) * move_speed
+	var current: Vector2 = Vector2(velocity.x, velocity.z)
+	var result: Vector2 = current.move_toward(target, acceleration * delta)
+	
+	velocity.x = result.x
+	velocity.z = result.y
+	
+	move_and_slide()
+	send_data.rpc(global_position)
+	
+@rpc("authority", "call_remote", "unreliable_ordered")
+func send_data(pos: Vector3) -> void:
+	global_position = pos
